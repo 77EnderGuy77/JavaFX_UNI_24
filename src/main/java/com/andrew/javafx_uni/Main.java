30f48c4a99575e189b71920de215213e3dd5402c
77EnderGuy77 <134065941+77EnderGuy77@users.noreply.github.com>
2024-04-10 03:40:48 +0300

    v2.0 Finish on Lab Work #3

--- src/main/java/com/andrew/javafx_uni/Main.java
+++ src/main/java/com/andrew/javafx_uni/Main.java
@@ -3,27 +3,18 @@ package com.andrew.javafx_uni;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import oshi.SystemInfo;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        SystemInfo sysInfo = new SystemInfo();
+        FXMLLoader root =  new FXMLLoader(getClass().getResource("/view.fxml"));
 
-        Label OSlabel = new Label("OS: " + sysInfo.getOperatingSystem());
-        Label RAMlabel = new Label("RAM: " + sysInfo.getHardware().getMemory().getTotal());
-        Label CPUlable = new Label("CPU: " + sysInfo.getHardware().getProcessor());
-
-        VBox root = new VBox(10);
-        root.getChildren().addAll(OSlabel, RAMlabel, CPUlable);
-
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Laba 2");
+        Scene scene = new Scene(root.load());
+        stage.setResizable(true);
+        stage.setTitle("Laba 3");
         stage.setScene(scene);
         stage.show();
     }
